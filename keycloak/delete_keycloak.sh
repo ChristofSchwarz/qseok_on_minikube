a4816e5d0bc2de4a4896681b7ddb45120ab48332
Christof Schwarz <christof.schwarz@gmx.at>
2019-10-08 08:40:36 +0200

    Create delete_keycloak.sh

---
+++ keycloak/delete_keycloak.sh
@@ -0,0 +1,6 @@
+kubectl delete service keycloak-svc
+kubectl delete deployment keycloak
+kubectl delete service postgres-svc
+kubectl delete deployment postgres
+kubectl delete configmap postgres-config 
+kubectl delete pvc pvc-postgres
