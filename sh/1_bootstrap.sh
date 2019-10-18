9ef348fe42e35c201203cb524c5e41c13f087ba3
Christof Schwarz <christof.schwarz@gmx.at>
2019-10-18 11:38:23 +0200

    Update 1_bootstrap.sh

--- sh/1_bootstrap.sh
+++ sh/1_bootstrap.sh
@@ -1,12 +1,18 @@
 echo 'executing "1_bootstrap.sh"'
 echo 'Updating Ubuntu'
-sudo apt-get -qq -y update
+#sudo apt-get -qq -y update
 
 echo 'Installing git nfs-kernel-server'
-sudo apt-get install -qq git nfs-kernel-server
+#sudo apt-get install -qq git nfs-kernel-server
 
 echo 'Disabling swap'
 sudo swapoff -a
+
+echo "copying yaml and keycloak folder to vagrant's home folder"
+mkdir /home/vagrant/yaml
+cp -R /vagrant/yaml/* /home/vagrant/yaml
+mkdir /home/vagrant/keycloak
+cp -R /vagrant/keycloak/* /home/vagrant/keycloak
 
 # Comment the swap line from fstab - permanently disable swap
 sudo sed -i.bak '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
@@ -20,8 +26,8 @@ sudo bash -c 'cat << EOF >>/etc/exports
 /export/src  *(rw,sync,no_subtree_check,no_root_squash)
 /export       *(rw,fsid=0,no_subtree_check,sync)
 EOF'
-
+echo 'Starting NFS Kernel server'
 sudo service nfs-kernel-server restart
 
-echo 'Adding name server'
-sudo  sed -i '1s/^/nameserver 8.8.8.8 /' /etc/resolv.conf
+#echo 'Adding name server'
+#sudo  sed -i '1s/^/nameserver 8.8.8.8 /' /etc/resolv.conf
