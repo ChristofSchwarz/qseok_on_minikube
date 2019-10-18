8f10f208b83c75306fa4049409ad7e0a6900d3f1
Christof Schwarz <christof.schwarz@gmx.at>
2019-10-18 15:44:48 +0200

    Update install_keycloak.sh

--- keycloak/install_keycloak.sh
+++ keycloak/install_keycloak.sh
@@ -4,4 +4,11 @@ kubectl wait --for=condition=available --timeout=3600s deployment/postgres
 kubectl create -f keycloak-depl-persist.yaml -f keycloak-svc.yaml
 echo 'Waiting until keycloak is ready ...'
 kubectl wait --for=condition=available --timeout=3600s deployment/keycloak
-echo 'Keycloak should now be ready on http://192.168.56.234:32080 in the next 2 minutes'
+
+KEYCLOAKURL="http://192.168.56.234:32080"
+echo 'Keycloak should now be ready on $KEYCLOAKURL in the next 2 minutes'
+until $(curl -s --output /dev/null --connect-timeout 5 --max-time 6 --head --fail $KEYCLOAKURL/auth); do
+    echo "waiting for response at $KEYCLOAKURL/auth"
+    sleep 5
+done
+echo 'Now ready.'
