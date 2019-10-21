8e9e9ba6fdd1df60e6c453db8e6866794f49e94c
Christof Schwarz <christof.schwarz@gmx.at>
2019-10-21 17:49:38 +0200

    upd

--- sh/9_qliksense.sh
+++ sh/9_qliksense.sh
@@ -1,10 +1,10 @@
 echo 'executing "9_qliksense.sh" ...'
 #
-#echo 'Adding stable and edge repo from qlik.bintray.com'
-#helm repo add qlik-stable https://qlik.bintray.com/stable
-#helm repo add qlik-edge https://qlik.bintray.com/edge
-#helm init
-#helm repo update
+echo 'Adding stable and edge repo from qlik.bintray.com'
+helm repo add qlik-stable https://qlik.bintray.com/stable
+helm repo add qlik-edge https://qlik.bintray.com/edge
+helm init
+helm repo update
 #
 echo 'installing stable "qliksense-init"'
 helm upgrade --install qlikinit qlik-stable/qliksense-init
