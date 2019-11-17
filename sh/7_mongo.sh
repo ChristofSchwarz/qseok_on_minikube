56f9c163608214fffb0f43f1d7ac2751f693d925
Christof Schwarz <christof.schwarz@gmx.at>
2019-11-17 16:25:48 +0100

    Update 7_mongo.sh

--- sh/7_mongo.sh
+++ sh/7_mongo.sh
@@ -4,6 +4,4 @@ echo 'Creating PVC for MongoDB'
 kubectl apply -f ~/yaml/pvc_mongo.yaml
 
 echo 'Installing MongoDB chart'
-#helm init
-#helm repo update
 helm install -n mongo stable/mongodb -f ~/yaml/mongo.yaml 
