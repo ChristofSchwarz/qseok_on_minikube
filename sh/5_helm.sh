8723ec6462b375ebc3e2f3bbd9c43af00e5f6854
Christof Schwarz <christof.schwarz@gmx.at>
2019-11-17 16:24:16 +0100

    Update 5_helm.sh

--- sh/5_helm.sh
+++ sh/5_helm.sh
@@ -1,8 +1,8 @@
 echo 'executing "5_helm.sh" ...'
 curl -s https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > get_helm.sh
 chmod 700 get_helm.sh
-# changed on 21-05-2019: helm version 2.14 doesn't work, getting an older version
-./get_helm.sh --version v2.13.1
+#./get_helm.sh --version v2.13.1
+./get_helm.sh --version v2.16.1
 
 #Initialize Helm Tiller pod, upgrade and update the repos
 helm init
